411c7484951201a4c00ba4a5fc8bc2df263944e0
fadlullah <fadlullahbugaje@gmail.com>
2024-02-09 14:21:44 +0100

    commit 4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,12 +32,12 @@
                     </div>
                 </div>
             </header>
-            <h1 data-aos="fade" data-aos-easing="linear" data-aos-duration="1000" data-aos-once="true" class="display-3 text-white font-weight-bold my-5">
+            <h1 data-aos="fade" data-aos-easing="linear" data-aos-duration="1000" data-aos-once="true" class="display-4 text-white font-weight-bold my-5">
             	A New Way<br>
             	To Secure Electric Grid
             </h1>
             <p data-aos="fade" data-aos-easing="linear" data-aos-duration="1000" data-aos-once="true" class="lead text-white my-4">
-              
+              ElectriSafe, offers a smart electronic method for licensing poles,<br>this solution provides real time, cost-effective tracking system to ensure proper safety
             </p>
             <a href="#contact" data-aos="fade" data-aos-easing="linear" data-aos-duration="1000" data-aos-once="true" class="btn my-4 font-weight-bold atlas-cta cta-green">Get Started</a>
         </div>
