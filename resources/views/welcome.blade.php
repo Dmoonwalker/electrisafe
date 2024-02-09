c40d189958ad618f14958a98fe19e0b73cb788f8
fadlullah <fadlullahbugaje@gmail.com>
2024-02-09 12:55:56 +0100

    commit 4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Bootstrap Template Atlas</title>
+    <title> ElectriSafe| Home </title>
     <meta name="description" content="Free bootstrap template Atlas">
     <link rel="icon" href="img/logo.png" sizes="32x32" type="image/png">
     <!-- custom.css -->
